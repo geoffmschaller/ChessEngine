1a191c471818fc88e3ba1ff21408d1a2e3b1fbc5
Geoff Schaller <geoffmschaller@gmail.com>
2023-04-24 20:42:34 -0600

    Board Testing

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int
 main()
 {
-	printf("Hello, World!\n");
+	bool game_running = true;
+	int GameBoard[64] = {1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 18};
+	printf("%d", GameBoard[2]);
+	while (game_running)
+	{
+		game_running = false;
+	}
 	return 0;
 }
